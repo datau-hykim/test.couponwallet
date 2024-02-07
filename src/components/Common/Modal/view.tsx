398f45a4649444d630d6be4d80d6f018b468f425
[김민식][기술개발본부][대리] <142783357+minsikkkkk@users.noreply.github.com>
2024-02-07 15:22:51 +0900

    레이아웃 구조 설정 및 GlobalStyle 적용

--- src/components/Common/Modal/view.tsx
+++ src/components/Common/Modal/view.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import styled from 'styled-components'
 import { ModalStoreType } from 'src/store/zustand'
-import { ModalInner, ModalWrap } from 'src/styles/Common/modal_styled'
 
 const Alert = (props?: ModalStoreType) => {
   const { title, closeModal, okText, onOkHandler, content } = props as ModalStoreType
@@ -31,3 +31,32 @@ const Confirm = (props?: ModalStoreType) => {
   )
 }
 export { Alert, Confirm }
+
+
+
+const ModalWrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  position: fixed;
+  z-index: 9999;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`
+
+const ModalInner = styled.div`
+  width: 300px;
+  padding: 30px;
+  background-color: #fff;
+  border-radius: 20px;
+  display: block;
+  flex-direction: column;
+  gap: 20px;
+`
+
+export { ModalInner, ModalWrap }
--- src/layout/PageLayout/view.tsx
+++ src/layout/PageLayout/view.tsx
@@ -1,12 +1,25 @@
-import { Outlet, ScrollRestoration } from 'react-router-dom'
-import { PageLayoutProps } from 'src/layout/PageLayout/index'
+import {Outlet, ScrollRestoration} from 'react-router-dom'
+import {PageLayoutProps} from 'src/layout/PageLayout/index'
+import Header from "src/layout/Header/view";
+import Footer from "src/layout/Footer/view";
+import Tabbar from "src/layout/TabBar/view";
+import styled from "styled-components";
 
 const PageLayoutView = ({}: PageLayoutProps) => {
-  return (
-    <>
-      <ScrollRestoration />
-      <Outlet />
-    </>
-  )
+    return (
+        <>
+            <ScrollRestoration/>
+            <Header/>
+            <ContentsLayout>
+                <Outlet/>
+            </ContentsLayout>
+            <Tabbar/>
+            <Footer/>
+        </>
+    )
 }
 export default PageLayoutView
+
+const ContentsLayout = styled.div`
+
+`
